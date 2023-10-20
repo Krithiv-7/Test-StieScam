03d072dcb2757370c73186c5e2fbb88140453355
KRITI_7 <krithivjayaprakash@gmail.com>
2023-10-20 21:21:02 +0530

    df

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-var buttons = document.querySelectorAll('.btn, .backspace');
+var buttons = document.querySelectorAll('.btn, .backspace, .Enter');
 var inputBox = document.querySelector('.input-box input');
+var showButton = document.querySelector('.show');
+
+// Variable to track whether the values are currently hidden
+var valuesHidden = false;
 
 buttons.forEach(function(button) {
     button.addEventListener('click', function() {
@@ -8,6 +12,9 @@ buttons.forEach(function(button) {
         if (value === "Backspace") {
             // Handle Backspace button: Remove the last character from the input field
             inputBox.value = inputBox.value.slice(0, -1);
+        } else if (value === "Enter") {
+            // Handle Enter button: Save the form (you can replace this with your saving logic)
+            saveForm();
         } else {
             // Check if the input length is less than 6; if so, append the value
             if (inputBox.value.length < 6) {
@@ -28,13 +35,20 @@ function updateInputClass() {
         inputBox.classList.remove("valid-input");
     }
 }
-var inputBox = document.querySelector('.input-box input');
 
-inputBox.addEventListener('click', function() {
-    if (!inputBox.hasAttribute('readonly')) {
-        inputBox.setAttribute('readonly', 'readonly');
-        inputBox.removeAttribute('readonly');
-        inputBox.focus();
+function saveForm() {
+    // Replace this with your actual save form logic
+    // For example, you can submit the form or perform an AJAX request here.
+    // For demonstration purposes, we'll simply alert a message.
+    alert('Form saved!');
+}
+
+showButton.addEventListener('click', function() {
+    // Toggle the visibility of the values in the input field
+    if (valuesHidden) {
+        inputBox.type = 'password'; // Hide values with asterisks
+    } else {
+        inputBox.type = 'text'; // Show the actual values
     }
+    valuesHidden = !valuesHidden; // Toggle the flag
 });
-
